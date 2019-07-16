256638c0eb4c971adac5e03ae1ab3e36f2d3f8ce
Acilio Silva <eng.acilio@gmail.com>
2019-07-16 11:11:03 +0100

    model and gitignore update

--- model.py
+++ model.py
@@ -21,11 +21,23 @@ class migrationModel:
                             workItem = workDataArrayP[count]          
 
                             if( workItem[2] == item[5]):
-                                    result = result + '\n' + ( str(workItem[0]).replace('nan','') + ";" + str(workItem[1]).replace('nan','') + ";" + str(workItem[2]).replace('nan','') + ";" + str(workItem[3]).replace('nan','') + ";" + str(workItem[4]).replace('nan','') + ";" + str(workItem[5]).replace('nan','') + ";" + str(workItem[6]).replace('nan','') + ";" + str(item[0]).replace('nan','') + ";" + str(item[1]).replace('nan','') + ";" + str(item[2]).replace('nan','') + ";" + str(item[3]).replace('nan','') + ";" + str(item[4]).replace('nan','') + ";"+ str(item[5]).replace('nan','') + ";"+ str(item[6]).replace('nan','') + ";"+ str(item[7]).replace('nan','') + ";"+ str(item[8]).replace('nan','') + ";"+ str(item[9]).replace('nan','') + ";"+ str(item[10]).replace('nan','') + ";"+ str(item[11]).replace('nan','') + ";"+ str(item[12]).replace('nan',''))
+                                    result = result + ( str(workItem[0]).replace('nan','') + ";" + str(workItem[1]).replace('nan','') + ";" + str(workItem[2]).replace('nan','') + ";" + str(workItem[3]).replace('nan','') + ";" + str(workItem[4]).replace('nan','') + ";" + str(workItem[5]).replace('nan','') + ";" + str(workItem[6]).replace('nan','') + ";" + str(item[0]).replace('nan','') + ";" + str(item[1]).replace('nan','') + ";" + str(item[2]).replace('nan','') + ";" + str(item[3]).replace('nan','') + ";" + str(item[4]).replace('nan','') + ";"+ str(item[5]).replace('nan','') + ";"+ str(item[6]).replace('nan','') + ";"+ str(item[7]).replace('nan','') + ";"+ str(item[8]).replace('nan','') + ";"+ str(item[9]).replace('nan','') + ";"+ str(item[10]).replace('nan','') + ";"+ str(item[11]).replace('nan','') + ";"+ str(item[12]).replace('nan','')) + '\n'
                             else:
                                     result = 'names are different'
                                     break
                             count +=  1
+            return result
+    
+    def createResultForDifferentNames(self, workDataArrayP, migrationWorkDataArrayP):
+            result = ''
+            if((len(workDataArrayP)) == (len(migrationWorkDataArrayP))):
+                    count = 0
+                    for item in migrationWorkDataArrayP:
+                        workItem = workDataArrayP[count]          
+
+                        if( workItem[2] == item[5]):
+                                result = result + ( str(workItem[0]).replace('nan','') + ";" + str(workItem[1]).replace('nan','') + ";" + str(workItem[2]).replace('nan','') + ";" + str(workItem[3]).replace('nan','') + ";" + str(workItem[4]).replace('nan','') + ";" + str(workItem[5]).replace('nan','') + ";" + str(workItem[6]).replace('nan','') + ";" + str(item[0]).replace('nan','') + ";" + str(item[1]).replace('nan','') + ";" + str(item[2]).replace('nan','') + ";" + str(item[3]).replace('nan','') + ";" + str(item[4]).replace('nan','') + ";"+ str(item[5]).replace('nan','') + ";"+ str(item[6]).replace('nan','') + ";"+ str(item[7]).replace('nan','') + ";"+ str(item[8]).replace('nan','') + ";"+ str(item[9]).replace('nan','') + ";"+ str(item[10]).replace('nan','') + ";"+ str(item[11]).replace('nan','') + ";"+ str(item[12]).replace('nan','')) + '\n'
+                        count +=  1
             return result
 
     def createWorkArray(self, refNumber, dataArray, indexRefNumberComparisom = 0):
@@ -39,7 +51,7 @@ class migrationModel:
             return workArray
 
     def processListOfRefNumber(self, refNumberArray, migrationDataArray, rulesDataArray):
-            with open("result/result.csv","w", encoding="utf-8") as output:
+            with open("result/result.csv","w+", encoding="utf-8") as output:
                     for refItem in refNumberArray:
                             migrationWorkDataArray = self.createWorkArray( refItem, migrationDataArray, 1 )
                             workDataArray = self.createWorkArray( refItem, rulesDataArray )
@@ -51,8 +63,15 @@ class migrationModel:
                                                     output.write( result )
                                                     print(refItem + ' OK')
                                             else:
+                                                self.processRefWithNameDifferent(refItem, migrationWorkDataArray, workDataArray)
                                                 print(refItem + ' - > KO - names are different')
                                     else:
                                             print(refItem + ' - > KO - the numbers are different')
                             else:
                                     print(refItem + ' - > KO - cant find on migrated document')
+    
+    def processRefWithNameDifferent(self, refNumber, migrationWorkDataArray, workDataArray):
+            with open("result/result_with_names_differents.csv","w+", encoding="utf-8") as output:                    
+                result = self.createResultForDifferentNames(workDataArray , migrationWorkDataArray)
+                if( result != ''):
+                        output.write( result )
